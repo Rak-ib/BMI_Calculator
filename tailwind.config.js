da7e0dbbd2bdbd28b7a9f28ae0a83faabc19f326
Rak-ib <19rakib120@gmail.com>
2024-12-22 16:29:17 +0600

    set up ready for BMI calculator

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Include your main HTML file
+    "./**/*.{html,js,jsx,ts,tsx}", // Include all relevant files (JS, TS, JSX)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
